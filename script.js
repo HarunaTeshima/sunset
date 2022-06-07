bab12d876ba6d482b13cb54b05a51768201e29e0
toshiaki21 <toshiaki.fujioka21@gmail.com>
2022-06-07 19:28:42 +0900

    js編集しました

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 $(function(){  
-  // とっしー
-  // スライド
-  let index = 0;
-    let slideMax = $('.slide').length
-    $('.right-arrow').on('click',function () {
-      if (index !== slideMax-1) {
-        index++;
-        num = index * -1 * 100;
-        $('.slide-container').css({'transform':`translateX(${num}%)`});
-      }
-    })
+
   
-    $('.left-arrow').on('click',function () {
-      if (index !== 0) {
-        index--;
-        num = index * -1 * 100;
-        $('.slide-container').css({'transform':`translateX(${num}%)`})
-      }
-    })
   
+    
     // タブ
     // ①.tab-list liが押されたら押された要素のid名を取得(関数名はattr())
     $('.tab-list_shop li').on('click',function(){
